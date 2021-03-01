b200339a4d47918fe6dac77150dab59c5ab54689
thejaviertc <javiertcjobs@gmail.com>
2021-03-01 13:14:03 +0100

    Fixed className instead of class

--- src/components/Data.js
+++ src/components/Data.js
@@ -117,9 +117,9 @@ class Data extends Component {
                             )}
                     </div>
                 ) : (
-                        <div class="d-flex justify-content-center">
-                            <div class="spinner-border" role="status">
-                                <span class="sr-only">Loading...</span>
+                        <div className="d-flex justify-content-center">
+                            <div className="spinner-border" role="status">
+                                <span className="sr-only">Loading...</span>
                             </div>
                         </div>
                     )
